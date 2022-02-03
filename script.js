447e50a772cbe0087dc71c4403aad0701ed458d2
Jamie <james@areafamily.us>
2022-02-02 18:53:42 -0600

    Basic layout created. Building out JS functions

---
+++ script.js
@@ -0,0 +1,5 @@
+// Declare Canvas variable and grab it in the DOM
+const canvas = document.getElementById("canvas-top");
+// Declare CTX variable to store 2D rendering context (tool we use to paint on Canvas)
+const ctx = canvas.getContext("2d");
+
